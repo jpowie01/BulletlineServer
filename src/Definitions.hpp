9255641512532f5a359247d312e5a824eaad66ff
Jakub Powierza <jpowie01@gmail.com>
2016-04-14 12:36:33 +0200

    Clear the code

---
+++ src/Definitions.hpp
@@ -0,0 +1,14 @@
+/*
+ * Counter Strike 2D
+ *
+ * (c) 2016 Jakub Powierza & Karolina Olszewska
+ *
+ */
+
+#ifndef Definitions_hpp
+#define Definitions_hpp
+
+// Core definitions
+#define SERVER_PORT     51000
+
+#endif
